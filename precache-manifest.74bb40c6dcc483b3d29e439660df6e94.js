fff52b1e974cf0ac7994d1b81f4d31986590fdf7
Justin Perez <perezjrp936@gmail.com>
2021-11-09 14:23:01 -0500

    Updates

---
+++ precache-manifest.74bb40c6dcc483b3d29e439660df6e94.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cad89ebde023d30561e41c0b028ac8a",
+    "url": "/wk20c-react-portfolio/index.html"
+  },
+  {
+    "revision": "2438a298f724170b5f06",
+    "url": "/wk20c-react-portfolio/static/css/main.a1c5932f.chunk.css"
+  },
+  {
+    "revision": "7a7c406f16849259d62d",
+    "url": "/wk20c-react-portfolio/static/js/2.02876227.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/wk20c-react-portfolio/static/js/2.02876227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b12255597ab34c228e",
+    "url": "/wk20c-react-portfolio/static/js/3.ecef989e.chunk.js"
+  },
+  {
+    "revision": "2438a298f724170b5f06",
+    "url": "/wk20c-react-portfolio/static/js/main.9994d766.chunk.js"
+  },
+  {
+    "revision": "e34d4fa2ca08f7e16920",
+    "url": "/wk20c-react-portfolio/static/js/runtime-main.e1653831.js"
+  },
+  {
+    "revision": "333937a0b379e5d3167845ebda50cebd",
+    "url": "/wk20c-react-portfolio/static/media/about_me.333937a0.jpg"
+  },
+  {
+    "revision": "6de091cd5ae7bb4edc3d9c9052a62721",
+    "url": "/wk20c-react-portfolio/static/media/and chill logo.6de091cd.PNG"
+  },
+  {
+    "revision": "58baea8a4490fdb900a238b76bf94a08",
+    "url": "/wk20c-react-portfolio/static/media/run_buddy.58baea8a.PNG"
+  },
+  {
+    "revision": "e2abc48ad45772c58bdc819f3725002d",
+    "url": "/wk20c-react-portfolio/static/media/spot.e2abc48a.png"
+  },
+  {
+    "revision": "ef4b430c846cdb0408f60d14ce3e3491",
+    "url": "/wk20c-react-portfolio/static/media/techBlog.ef4b430c.PNG"
+  },
+  {
+    "revision": "8c89edd951d168155d731bd2a53e117d",
+    "url": "/wk20c-react-portfolio/static/media/weather.8c89edd9.PNG"
+  }
+]);
