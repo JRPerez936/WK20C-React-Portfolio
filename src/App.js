607c07dc35866817ea1a6673587ccba4349aaf61
Justin Perez <perezjrp936@gmail.com>
2021-11-08 21:53:14 -0500

    added nav and helpers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [categories] = useState([
+    {
+      name: 'Portfolio',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'Resume', description: 'Delicious delicacies' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
           <About></About>
       </main>
